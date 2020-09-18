ba9870334c8357c1e2040c0c65a7d009818f1d3d
yuenxm <yuenxm@qq.com>
2020-09-19 01:56:32 +0800

    修改2

--- app/src/main/java/com/example/myapplication2/ThirdActivity.java
+++ app/src/main/java/com/example/myapplication2/ThirdActivity.java
@@ -11,5 +11,6 @@ public class ThirdActivity extends Activity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+        int a1;
     }
 }
