6a427f6859edc6c965b1e00f36b2bc4f35a59c20
yuenxm <yuenxm@qq.com>
2020-09-19 01:56:10 +0800

    修改1

--- app/src/main/java/com/example/myapplication2/ThirdActivity.java
+++ app/src/main/java/com/example/myapplication2/ThirdActivity.java
@@ -1,6 +1,15 @@
 package com.example.myapplication2;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
 
 public class ThirdActivity extends Activity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
